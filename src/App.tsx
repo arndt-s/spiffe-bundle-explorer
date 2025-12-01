a43c87dd52d59c276634ede04d8a70ed2a1bf211
arndt-s <17650715+arndt-s@users.noreply.github.com>
2025-12-01 17:59:05 +0100

    use hash-based routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import { LandingPage } from './pages/LandingPage';
 import { BundleViewPage } from './pages/BundleViewPage';
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter basename="/spiffe-bundle-explorer">
+    <HashRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/bundle" element={<BundleViewPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
